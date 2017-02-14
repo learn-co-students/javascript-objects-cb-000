de91f8280ab09dd785a032d4b4269eb7717e8a15
Eric Lau <yasuyo369@gmail.com>
2017-02-14 18:43:20 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist = {
+  Song: "My Way",
+  artistName: "Frank Sinatra"
+};
+
+function updatePlaylist(playlist, artistName, title)
+{
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName)
+{
+
+delete artistName.dinner;
+}
