38448ca9410dc103bab3a19a288124284a5b7dcd
Drew Pham <drewvpham@gmail.com>
2017-04-30 21:42:30 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+playlist={artist:'song'}
+
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName]=songTitle
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
