ae0fa8daba58d62079610e146ff364fb4fdd6dc6
Chris Nunez <cenunez47@gmail.com>
2017-10-03 18:28:12 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { rkelly: 'Down Low'};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist;
+}
